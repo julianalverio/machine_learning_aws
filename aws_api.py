8c0ac2d06869dddae39ab12ccd9d17edb94d267a
julianalverio <julian.a.alverio@gmail.com>
2020-01-07 15:42:56 -0300

    change w to w+ for pem

--- aws_api.py
+++ aws_api.py
@@ -26,7 +26,7 @@ Assumptions about this directory:
 def generate_keypair():
     ec2 = boto3.resource('ec2')
     # outfile = open('ec2-keypair.pem', 'w')
-    outfile = open('ec2-keypair.pem', 'w')
+    outfile = open('ec2-keypair.pem', 'w+')
     key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
     KeyPairOut = str(key_pair.key_material)
     print(KeyPairOut)
