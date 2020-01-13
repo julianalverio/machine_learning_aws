c501911b2a2c77dde6dcfffefdfd79f9929d17e2
julianalverio <julian.a.alverio@gmail.com>
2020-01-13 01:47:28 -0300

    remove redundant pull

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-git -C /home/ubuntu/machine_learning_aws pull
 cp -r /home/ubuntu/machine_learning_aws/template /home/ubuntu/machine_learning_aws/daily_user
 tmux new-session -d 'conda activate conda_env && jupyter notebook --port=8888 --no-browser --ip="*" --NotebookApp.token="" --NotebookApp.password="" /home/ubuntu/machine_learning_aws/daily_user'
-
-
-
