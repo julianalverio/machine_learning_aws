d317351b94d4ed9aea9d691757c576d7e1f569af
julianalverio <julian.a.alverio@gmail.com>
2020-01-13 02:31:55 -0300

    adding small mod to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp -r /home/ubuntu/machine_learning_aws/template /home/ubuntu/machine_learning_aws/daily_user
+source ~/.bashrc
 tmux new -d -s jupyter
 tmux send-keys -t jupyter.0 'conda activate conda_env && jupyter notebook --port=8888 --no-browser --ip="*" --NotebookApp.token="" --NotebookApp.password="" /home/ubuntu/machine_learning_aws/daily_user' ENTER
