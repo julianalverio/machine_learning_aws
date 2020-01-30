33e70d9faedaec9b1bd9a9005b8ffbfff7bac15a
rmsander <rmsander@mit.edu>
2020-01-30 17:08:46 -0500

    Added docstrings/modified functionality for email_test.py

--- email_test.py
+++ email_test.py
@@ -1,3 +1,6 @@
+"""Script for testing emails that you plan to later send out to the entire
+class (in our case, this was login info for their AWS machines."""
+
 # Native Python imports
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -6,23 +9,12 @@ from email.mime.text import MIMEText
 import smtplib
 
 def test_mail_to_list(MSG_TYPE="restart"):
-    """Class method for writing to a set of emails determined by email
-    information from users.csv.
-
-    The information sent in this email provides information for users on
-    how to setup and get into their assigned AWS instances.
-
-    Returns:
-        1. A mapping from users to IP addresses that can be used for
-            later reference.
-    """
-
 
     # Message information
-    name_of_user = "Ryan Sander"
+    name_of_user = "TEST USER"
     ip_address = "0.00.000.00"
     fromaddr = "machinelearning.uruguay@gmail.com"
-    toaddr = "rmsander1026@gmail.com"
+    toaddr = "TEST_EMAIL @ DOMAIN"
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
@@ -30,106 +22,44 @@ def test_mail_to_list(MSG_TYPE="restart"):
 
     # Text body of message
     if MSG_TYPE == "full":
-        body = """\
-
-        Hola %s,
-
-        Below is your login information for this course.  
-        Mac users and users running Linux: Please
-        copy and paste the following command into your command line.
-        Windows users: paste the following command into Git Bash
+        body = """\n
+               
+               Hola %s,
 
 
-        Next, copy and paste this command:
-        ssh -o "StrictHostKeyChecking no" ubuntu@%s
+               Below is your login information for today.
+               Today's password: pantalones
 
-        Next, copy and paste these commands one at a time.
+               Windows users: please be sure to restart your computer every day before class.
+               Mac users and users running Linux: Please
+               copy and paste the following commands into
+               your command line.
+               Windows users: paste the following commands
+               into Git Bash, and please remember to RESTART or SHUT DOWN 
+               your computer before coming to class.
 
-        source ~/.bashrc
+               1. Set up ssh port forwarding:
+               ssh -o "StrictHostKeyChecking no" -NfL 5005:localhost:8888 ubuntu@%s
+               Then type the password.
 
+               If you get an error that says something like:
+               bind [127.0.0.1]:5005: Address already in use
 
-        Make sure you paste this command below in ONE line:
-        sudo /home/ubuntu/conda/bin/conda env create -f 
-        /home/ubuntu/machine_learning_aws/environment.yml -n conda_env
-
-        conda init bash
-
-        conda activate conda_env
-
-        jupyter notebook --port=8888 --no-browser --ip='*' 
-        --NotebookApp.token='' --NotebookApp.password='' 
-        /home/ubuntu/machine_learning_aws/daily_user
-
-
-        Paste this command:
-        ssh -NfL 5005:localhost:8888 ubuntu@%s
-
-
-        Finally, your web browser and type:
-        localhost:5005
-
-
-        This will take you to the Jupyter notebooks on AWS that we will 
-        be using for the rest of this course!  
-
-        Mucho amor,
-        GSL Uruguay Technical Team
-        """ % (name_of_user, ip_address, ip_address)
-
-    elif MSG_TYPE == "restart":
-        body = """\
-
-        Hola %s,
-
-        Below is your login information for this 
-        course.  
-
-        Mac users and users running Linux: Please
-        copy and paste the following commands into 
-        your command line.
-
-        Windows users: paste the following commands
-        into Git Bash
-
-        PASSWORD: pantalones
-
-        1. Connect to your machine:
-        ssh -o "StrictHostKeyChecking no" ubuntu@%s
+               Mac and Linux users, run this:
+               pkill -f 5005
+               Then retry step 1
+               
+               2. Go to your web browser (such
+               as Chrome) and type:
+               localhost:5005
+               
+               This will take you to your AWS Jupyter
+               notebooks! If this worked for you, you're all set!
 
 
-        2. Next, we want to initialize our conda environment:
-        conda activate conda_env
-
-
-        3. Now, we want to install tmux in case we lose connection:
-        sudo apt-get install tmux
-
-
-        4. Now we want to start a tmux session:
-        tmux
-
-
-        5. Next, open a Jupyter notebook:
-        jupyter notebook --port=8888 --no-browser --ip='*' --NotebookApp.token='' --NotebookApp.password='' /home/ubuntu/machine_learning_aws/daily_user
-
-
-        6. Next, detach from your tmux session:
-            PRESS (1) ctrl + b (same time), 
-             then (2) d (after) on your keyboard
-
-
-        7. (ON YOUR LOCAL MACHINE) Use ssh port forwarding:
-        ssh -NfL 5005:localhost:8888 ubuntu@%s
-
-        8. Finally, go to your web browser (such as Chrome) and type:
-        localhost:5005
-
-
-        This will take you to your AWS Jupyter notebooks!
-
-        Mucho amor,
-        GSL Uruguay Technical Team
-        """ % (name_of_user, ip_address, ip_address)
+               Mucho amor,
+               GSL Uruguay Technical Team
+               """ % (name_of_user, ip_address)
 
     # Prepare email to server information
     msg.attach(MIMEText(body, 'plain'))
@@ -142,8 +72,10 @@ def test_mail_to_list(MSG_TYPE="restart"):
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
 
+
 def main():
     test_mail_to_list()
+
 
 if __name__ == "__main__":
     main()
